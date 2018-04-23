00d32ad132dd77211e61cc3321dfefebdae51772
t1240963024 <1240963024@qq.com>
2018-04-23 15:55:57 +1000

    Add files via upload

--- OSMproject/src/Handle/MaxSpeedRelationWay.java
+++ OSMproject/src/Handle/MaxSpeedRelationWay.java
@@ -1,7 +1,6 @@
 package Handle;
 
 import java.io.File;
-import java.util.ArrayList;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
@@ -11,53 +10,34 @@ import org.dom4j.DocumentException;
 import org.dom4j.Element;
 import org.dom4j.io.SAXReader;
 
-public class Handle_Main {
+public class MaxSpeedRelationWay {
+
+	static final Logger LOG = Logger.getLogger(MaxSpeedRelationWay.class.getName());
 	
-	static final Logger LOG = Logger.getLogger(Handle_Main.class.getName());
+	Way way;
 	
-	static List<Way> ways;
-	
-	public static void main(String args[]) {
-		/*
-		if(args == null || args.length == 0) {
-		//	LOG.log(Level.SEVERE, "you should input what kind of problem you want");
-			System.err.println("Missing argument");
-		}else {
-			getWays();
-			if(args[0].equals("max speed problem")) {
-				int speed_gap = Integer.parseInt(args[1]);
-				MaxSpeedGapProblem.getInstance().inCorrectMaxSpeed(ways, speed_gap);
-			}
-		}
-		*/
-		getWays();
-		MaxSpeedGapProblem.getInstance().inCorrectMaxSpeed(ways, 60);
-		
-	}
-	
-	public static void getWays() {
-		
-		int count_maxspeed = 0;
-		int count_streetNameNull = 0;
-		ways = new ArrayList<>();
-		Way way;
+	public void getRelation() {
 		
 		SAXReader reader = new SAXReader();
 		try {	
-			Document document = reader.read(new File("src/res/way.xml"));
+			Document document = reader.read(new File("src/res/relation.xml"));
 			Element root = document.getRootElement();
 			List<Element> list = root.elements();
 			
+			Relation relation;
+			
 			for(Element e:list) {
-
+				
+				relation = new Relation();
 				way = new Way();
 				way.id = Integer.parseInt(e.attributeValue("id"));
+				relation.id = Integer.parseInt(e.attributeValue("id"));
 				List<Element> cList = e.elements();
 				for(Element c:cList) {
-					if(c.getName().equals("nd")) {
+					if(c.getName().equals("maxspeed")) {
 						way.nodes_ref.add(c.attributeValue("ref"));
 						
-					}else if(c.getName().equals("tag")) {
+					}else if(c.getName().equals("way")) {
 						if(c.attributeValue("k").equals("maxspeed")) {
 							count_maxspeed++;
 							way.maxspeed = Integer.parseInt(c.attributeValue("v"));
@@ -82,4 +62,5 @@ public class Handle_Main {
 			LOG.log(Level.INFO, "how many ways have max speed: " + count_maxspeed);
 		}
 	}
+	
 }
--- OSMproject/src/Handle/Relation.java
+++ OSMproject/src/Handle/Relation.java
@@ -1,5 +1,17 @@
 package Handle;
 
+import java.util.HashMap;
+
 public class Relation {
+	
+	public int id;
+	//role: forward outer inner backward link
+	HashMap<Integer, String> ways; 
+	
+	
+	
+	public Relation() {
+		
+	}
 
 }
