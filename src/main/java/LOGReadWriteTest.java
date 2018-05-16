2db0961a1ef6f17333c1adb3b963904d30a7869d
Yifeng Wang <s3574364@student.rmit.edu.au>
2018-05-16 14:12:37 +1000

    LOGReadWrite Junit test,but not finished yet

---
+++ src/main/java/LOGReadWriteTest.java
@@ -0,0 +1,17 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+import agentsoz.osm.analysis.handler.LOGReadWrite;
+
+public class LOGReadWriteTest {
+	
+		
+		private LOGReadWrite ins;
+		
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
