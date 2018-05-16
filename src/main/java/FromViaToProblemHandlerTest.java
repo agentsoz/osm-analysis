60c54858dd48972d70c140dcd3c66c0359b29a09
Yifeng Wang <s3574364@student.rmit.edu.au>
2018-05-16 14:03:10 +1000

    Junit test

---
+++ src/main/java/FromViaToProblemHandlerTest.java
@@ -0,0 +1,22 @@
+import static org.junit.Assert.*;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FromViaToProblemHandlerTest {
+	
+private String[] nodes;
+	
+	private String relationID;
+
+	@Before
+	public void setUp() throws Exception {
+	//	relationID = "16888";
+	//	nodes = {"","",""};
+	}
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
